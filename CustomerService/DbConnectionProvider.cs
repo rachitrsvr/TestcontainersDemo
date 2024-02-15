8349f738c2375d86be400bbd10585543967905a4
rachitrsvr <rachit.shah@rsvrtech.com>
2024-02-15 14:42:25 +0530

    Intial Commit

---
+++ CustomerService/DbConnectionProvider.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customers
+{
+    public sealed class DbConnectionProvider
+    {
+        private readonly string _connectionString;
+
+        public DbConnectionProvider(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public DbConnection GetConnection()
+        {
+            return new NpgsqlConnection(_connectionString);
+        }
+    }
+}
